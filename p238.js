c66bc89be31a48aa28e6c5ad5202b971773740d7
imdadulahmed1593 <ahmedimdad1593@gmail.com>
2024-01-14 13:20:43 +0600

    #p238 done

---
+++ p238.js
@@ -0,0 +1,32 @@
+// product-of-array-except-self
+
+var productExceptSelf = function (nums) {
+  const prefix = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i === 0) {
+      prefix[i] = 1;
+    } else {
+      prefix[i] = nums[i - 1] * prefix[i - 1];
+    }
+  }
+
+  const suffix = [];
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (i === nums.length - 1) {
+      suffix[i] = 1;
+    } else {
+      suffix[i] = nums[i + 1] * suffix[i + 1];
+    }
+  }
+
+  const result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix[i] * suffix[i];
+  }
+
+  return result;
+};
+console.log(productExceptSelf([1, 2, 3]));
