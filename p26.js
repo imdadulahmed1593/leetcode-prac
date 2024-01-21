f0fe63984eda85e83604b6681510c9b9cb6b9d5a
imdadulahmed1593 <ahmedimdad1593@gmail.com>
2024-01-21 10:05:14 +0600

    p26 done

---
+++ p26.js
@@ -0,0 +1,15 @@
+// 26. remove duplicates from sorted array
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+
+  let j = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[j] !== nums[i]) {
+      nums[++j] = nums[i];
+    }
+  }
+
+  return j + 1;
+};
