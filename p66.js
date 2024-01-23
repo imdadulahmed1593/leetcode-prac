8abc68dd0c92b99328ad8e6b7e33c6a35e4c1df9
imdadulahmed1593 <ahmedimdad1593@gmail.com>
2024-01-23 20:46:47 +0600

    p66 done

---
+++ p66.js
@@ -0,0 +1,20 @@
+// 66. Plus one
+var plusOne = function (digits) {
+  let r = digits.length - 1;
+
+  while (true) {
+    if (r < 0) {
+      digits.unshift(1);
+      return digits;
+    }
+
+    if (digits[r] + 1 === 10) {
+      digits[r] = 0;
+    } else {
+      digits[r] += 1;
+      return digits;
+    }
+
+    r--;
+  }
+};
